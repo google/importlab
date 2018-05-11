8ed21e7683a64c200bba4ef348c3974769d5ad65
Martin DeMello <mdemello@google.com>
2018-05-11 14:13:06 -0700

    use x.y.z versioning

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ URL = 'https://github.com/google/importlab'
 EMAIL = 'pytype-dev@google.com'
 AUTHOR = 'Google Inc.'
 REQUIRES_PYTHON = '>=2.7.0'
-VERSION = '0.1'
+VERSION = '0.1.0'
 
 REQUIRED = [
     'networkx',
