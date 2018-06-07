1e797ebcd1774f3cdeac6f4de7895ea6a4b35f3e
Martin DeMello <mdemello@google.com>
2018-06-06 17:05:21 -0700

    bump version to 0.2 and add a changelog

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ URL = 'https://github.com/google/importlab'
 EMAIL = 'pytype-dev@google.com'
 AUTHOR = 'Google Inc.'
 REQUIRES_PYTHON = '>=2.7.0'
-VERSION = '0.1.1'
+VERSION = '0.2.0'
 
 REQUIRED = [
     'networkx',
