ec89e2e85b4362c9b8a08dac58bdc4b66611372b
Martin DeMello <mdemello@google.com>
2018-08-23 14:18:50 -0700

    add directory support

--- importlab/utils.py
+++ importlab/utils.py
@@ -46,6 +46,45 @@ def expand_paths(paths, cwd=None):
     return [expand_path(x, cwd) for x in paths]
 
 
+def collect_files(path, extension):
+  """Collect all the files with extension in a directory tree."""
+
+  # We should only call this on an actual directory; callers should do the
+  # validation.
+  assert os.path.isdir(path)
+  out = []
+  # glob would be faster (see PEP471) but python glob doesn't do **/*
+  for root, _, files in os.walk(path):
+    out += [os.path.join(root, f) for f in files if f.endswith(extension)]
+  return out
+
+
+def expand_source_files(filenames, cwd=None):
+  """Expand a list of filenames passed in as sources.
+
+  This is a helper function for handling command line arguments that specify a
+  list of source files and directories.
+
+  Any directories in filenames will be scanned recursively for .py files.
+  Any files that do not end with ".py" will be dropped.
+
+  Args:
+    filenames: A list of filenames to process.
+    cwd: An optional working directory to expand relative paths
+  Returns:
+    A set of full paths to .py files
+  """
+  out = []
+  for f in expand_paths(filenames, cwd):
+    if os.path.isdir(f):
+      # If we have a directory, collect all the .py files within it.
+      out += collect_files(f, ".py")
+    else:
+      if f.endswith(".py"):
+        out.append(f)
+  return set(out)
+
+
 def split_version(version):
     return tuple([int(v) for v in version.split('.')])
 
