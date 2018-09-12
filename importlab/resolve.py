3aede508a1bc698e45ff04f46e2abc1173f93a2b
Teddy Sudol <magesolumin@gmail.com>
2018-09-12 15:24:58 -0700

    Strip `__init__` from inferred module names.
    Closes #22 and closes google/pytype#154.

--- importlab/resolve.py
+++ importlab/resolve.py
@@ -102,6 +102,8 @@ def infer_module_name(filename, fspath):
     for f in fspath:
         short_name = f.relative_path(filename)
         if short_name:
+            if short_name.endswith("/__init__"):
+                short_name = short_name[:short_name.rfind('/')]
             return short_name.replace(os.path.sep, '.')
     # We have not found filename relative to anywhere in pythonpath.
     return ''
